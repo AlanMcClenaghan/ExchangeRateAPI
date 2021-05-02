f198c53d8cc7396324ee77e9d4e56540588e2644
Alan McClenaghan <alandmcclenaghan@icloud.com>
2021-05-02 16:15:42 +0100

    UI elements and Handlers

--- force-app/main/default/lwc/serverSideHTTP/serverSideHTTP.js
+++ force-app/main/default/lwc/serverSideHTTP/serverSideHTTP.js
@@ -1,3 +1,42 @@
 import { LightningElement } from 'lwc';
+import exchangeRateData from '@salesforce/apex/ExchangeRateAPIController.retrieveExchangeRates'
 
-export default class ServerSideHTTP extends LightningElement {}
+export default class ServerSideHTTP extends LightningElement {
+
+    options = [
+        {label: 'Austrailia - AUD', value: 'AUD'},
+        {label: 'Canada - CAD', value: 'CAD'},
+        {label: 'China - CNY', value: 'CNY'},
+        {label: 'Eurozone - EUR', value: 'EUR'},
+        {label: 'India INR', value: 'INR'},
+        {label: 'Japan - JPY', value: 'JPY'},
+        {label: 'UK - GBP', value: 'GBP'},
+        {label: 'USA - USD', value: 'USD'}
+    ]
+
+    fromCurrencyValue = 'GBP';
+    toCurrencyValue;
+    fromQuery;
+    toQuery;
+    displayData;
+
+
+   fromCurrencyChangeHandler(event) {
+        this.fromCurrencyValue = event.target.value;
+        console.log('fromCurrencyValue: ' + this.fromCurrencyValue); 
+    }
+
+    toCurrencyChangeHandler(event) {
+        this.toCurrencyValue = event.target.value;
+        console.log('toCurrencyValue: ' + this.toCurrencyValue); 
+    }
+
+    currencyConversionHandler() {
+        console.log('fromCurrencyValue: ' + 
+                    this.fromCurrencyValue + 
+                    ' ' +
+                    'toCurrencyValue: ' + 
+                    this.toCurrencyValue );
+    }
+
+}
